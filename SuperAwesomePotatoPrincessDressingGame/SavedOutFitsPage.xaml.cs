bb385b57aa81a3fbae307040d64b9ca895525d52
Maiju Schreck <K2322@student.jamk.fi>
2016-04-08 13:06:00 +0300

    Savedoutfits AGAIN

---
+++ SuperAwesomePotatoPrincessDressingGame/SavedOutFitsPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SuperAwesomePotatoPrincessDressingGame
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SavedOutFitsPage : Page
+    {
+        public SavedOutFitsPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void LeftButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RightButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void QuitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
